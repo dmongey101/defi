c346bb3fd66c1e65ac32df7f571c59d19c02416c
Donal Mongey <donalmongey@gmail.com>
2021-05-31 21:27:16 +0100

    Connecting to metamask

---
+++ client/src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { useWeb3 } from '@openzeppelin/network/react';
+import Web3Info from './components/Web3Info';
+
+const infuraProjectId = '95202223388e49f48b423ea50a70e336';
+
+function App() {
+  const web3Context = useWeb3(`wss://mainnet.infura.io/ws/v3/${infuraProjectId}`);
+  const { networkId, networkName, providerName } = web3Context;
+
+  return (
+    <div className="App">
+      <div>
+        <h1>OpenZeppelin Network.js</h1>
+        <Web3Info title="Web3 Info" web3Context={web3Context} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
